2fa10358f4f9bc8fccbf8879bf734db2f9bec722
Dhafia06 <dhafia421@gmail.com>
2025-09-12 21:38:39 +0530

    Update __manifest__.py

--- addons/legal_case_mgmt/__manifest__.py
+++ addons/legal_case_mgmt/__manifest__.py
@@ -7,6 +7,11 @@
     'description': "A minimal app to manage legal cases, clients, lawyers, and hearings.",
     'data': [
         'security/ir.model.access.csv',
+        'data/sequence.xml',
+        'reports/case_report.xml',  # <-- ADD THIS LINE
+        'views/legal_case_views.xml',
+        'views/legal_hearing_views.xml',
+        'views/res_partner_views.xml',
         'views/menus.xml',
     ],
     'application': True,
